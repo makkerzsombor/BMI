244f2ffb64fc6859258e4b33100a60b4e3d4cc3a
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-20 11:45:47 +0200

    szamitas working

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function szamit() {
-    let mag = parseInt(document.getElementById('cm'))
-    
+    let mag = parseInt(document.getElementById('cm').value)
+    let suly = parseInt(document.getElementById('kg').value)
+    document.getElementById('sajatbmi').textContent = Math.round((suly / (mag / 100) ** 2)* 100 / 100);
 }
 document.getElementById('szamitas').addEventListener('click',szamit)
