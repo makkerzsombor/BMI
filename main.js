53a54b0cbbc3216b4d5a4397df5c4c174a6de92b
makkerzsombor <makkerzsombor@users.noreply.github.com>
2022-09-20 10:04:09 +0000

    deploy: b23ae60d804f4bc676e5ed9d245d251fef96eeea

--- main.js
+++ main.js
@@ -3,6 +3,24 @@ function szamit() {
     let suly = parseInt(document.getElementById('kg').value)
     let ered = suly / (mag / 100) ** 2;
     document.getElementById('sajatbmi').textContent = 'Az ön BMI értéke: ' + ered.toFixed(2);
-    
+    if (ered.toFixed(2)<=18.5) {
+        let element = document.getElementById("sovany");
+        element.classList.add("sargatable");
+    }else if (ered.toFixed(2)<=24.9) {
+        let element = document.getElementById("normal");
+        element.classList.add("sargatable");
+    }else if(ered.toFixed(2)<=29.9){
+        var element = document.getElementById("tulsuly");
+        element.classList.add("sargatable");
+    }else if(ered.toFixed(2)<=34.9){
+        var element = document.getElementById("tul1");
+        element.classList.add("sargatable");
+    }else if(ered.toFixed(2)<=39.9){
+        var element = document.getElementById("tul2");
+        element.classList.add("sargatable");
+    }else{
+        var element = document.getElementById("tul3");
+        element.classList.add("sargatable");
+    }
 }
 document.getElementById('szamitas').addEventListener('click',szamit)
