f1dbcb8368dfc43f6219a26f210c01007290658c
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-20 11:53:35 +0200

    default szamitas works

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function szamit() {
     let mag = parseInt(document.getElementById('cm').value)
     let suly = parseInt(document.getElementById('kg').value)
-    document.getElementById('sajatbmi').textContent = Math.round((suly / (mag / 100) ** 2)* 100 / 100);
+    let ered = suly / (mag / 100) ** 2;
+    document.getElementById('sajatbmi').textContent = 'Az ön BMI értéke: ' + ered.toFixed(2);
+    
 }
 document.getElementById('szamitas').addEventListener('click',szamit)
