b995b1cd1eb2467cfdf876d9c1a94b8e1246df4b
makkerzsombor <makkerzsombor@users.noreply.github.com>
2022-09-20 10:54:36 +0000

    deploy: dfb030502f07a2f43a35af2a840e2f911c93f8bd

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-function szamit() {
-   
-
-    let mag = parseInt(document.getElementById('cm').value)
-    let suly = parseInt(document.getElementById('kg').value)
-    let ered = suly / (mag / 100) ** 2;
-    document.getElementById('sajatbmi').textContent = 'Az ön BMI értéke: ' + ered.toFixed(2);
-
-    let low = (18.5*(mag/100)**2).toFixed(1);
-    let high = (24.9*((mag/100)**2)).toFixed(1);
-    let atlag = ((low + high)/2);
-    document.getElementById("idealisbmi").innerHTML = "Az ideális testtömege: " + atlag + " kg";
-    document.getElementById("kozott").innerHTML = low + " - " + high + " kg";
-    console.log(atlag);
-
+function szamit() {   
+    
     var element = document.getElementById("sovany");
     element.classList.remove("sargatable");
     var element = document.getElementById("normal");
@@ -25,6 +12,18 @@ function szamit() {
     element.classList.remove("sargatable");
     var element = document.getElementById("tul3");
     element.classList.remove("sargatable");
+
+    let mag = parseInt(document.getElementById('cm').value)
+    let suly = parseInt(document.getElementById('kg').value)
+    let ered = suly / (mag / 100) ** 2;
+    document.getElementById('sajatbmi').textContent = 'Az ön BMI értéke: ' + ered.toFixed(2);
+
+    let low = (18.5*(mag/100)**2).toFixed(1);
+    let high = (24.9*((mag/100)**2)).toFixed(1);
+    let atlag = ((low + high)/2);
+    document.getElementById("idealisbmi").innerHTML = "Az ideális testtömege: " + atlag + " kg";
+    document.getElementById("kozott").innerHTML = low + " - " + high + " kg";
+    console.log(atlag);    
 
     if (ered.toFixed(2)<=18.5) {
         element = document.getElementById("sovany");
@@ -45,7 +44,5 @@ function szamit() {
         element = document.getElementById("tul3");
         element.classList.add("sargatable");
     }
-
-    
 }
 document.getElementById('szamitas').addEventListener('click',szamit)
